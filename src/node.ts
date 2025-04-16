aa49da35ca5c387a8a63b574987cd163b8d80428
Matt McCormick <matt@fideus.io>
2025-04-15 20:26:17 -0400

    feat: add nodeSchema

---
+++ src/node.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const nodeSchema = z.object({
+  type: z.string().describe("identifier for node variant"),
+  data: z.record(z.any()).optional().describe(
+    "information associated by the ecosystem with the node; never specified by mdast",
+  ),
+  position: z
+    .object({
+      start: z.object({
+        line: z.number().describe("line in the source file, 1-indexed"),
+        column: z.number().describe("column in the source file, 1-indexed"),
+        offset: z.number().optional().describe(
+          "offset character in the source file, 0-indexed",
+        ),
+      }).describe("place of first character of parsed source region"),
+      end: z.object({
+        line: z.number().describe("line in the source file, 1-indexed"),
+        column: z.number().describe("column in the source file, 1-indexed"),
+        offset: z.number().optional().describe(
+          "offset character in the source file, 0-indexed",
+        ),
+      }).describe("place of first character after parsed source region"),
+      indent: z
+        .array(z.number())
+        .optional().describe(
+          "start column at each index in the source region, for elements that span multiple lines",
+        ),
+    })
+    .optional().describe(
+      "location of node in source file; must not be present for generated nodes",
+    ),
+}).describe(
+  "Base node object, based on the [unist](https://github.com/syntax-tree/unist) syntax tree.",
+);
+
+export type Node = z.infer<typeof nodeSchema>;
